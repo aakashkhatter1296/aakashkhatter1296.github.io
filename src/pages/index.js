abc00fec6b6f8423685d6e607da858e053d5dcac
Aakash Khatter <khatteraakash1996@gmail.com>
2020-10-30 22:13:16 +0530

    Add scss support, lato font, normalize.css

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+// import reset styles and global styles
+import "./styles/normalize.css"
+import "./styles/globalStyles.scss"
+// import styles for index file
+import styles from "./styles/index.module.scss"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div className={styles.comingSoonContainer}>
+      <h1>Something big coming soon!</h1>
+    </div>
+  )
 }
